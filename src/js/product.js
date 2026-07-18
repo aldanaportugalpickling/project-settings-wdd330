a8a5448dcf2d300586dfdbf2724ab7fe3adcf407
AldanaPortugalPickling <aldanapickling@gmail.com>
2026-07-18 03:32:46 -0500

    added

--- src/js/product.js
+++ src/js/product.js
@@ -3,7 +3,7 @@ import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 const productId = getParam("product");
-console.log("productId");
+console.log(productId);
 
 function addProductToCart(product) {
   const cartItems = getLocalStorage("so-cart") || [];
